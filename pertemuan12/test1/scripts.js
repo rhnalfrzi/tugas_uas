3c851339b632b93172ce6dda4ed3c1704d5496b8
rhnalfrzi <raihanalfarizi146@gmail.com>
2024-07-03 23:22:33 +0700

    first init github

---
+++ pertemuan12/test1/scripts.js
@@ -0,0 +1,18 @@
+document.getElementById('registrationForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const username = document.getElementById('username').value;
+    const email = document.getElementById('email').value;
+    const alamat = document.getElementById('alamat').value;
+    const password = document.getElementById('password').value;
+    const confirmPassword = document.getElementById('confirmPassword').value;
+
+    if (password !== confirmPassword) {
+        alert('Passwords do not match');
+        return;
+    }
+
+    // If you want to do further processing or send data to the server, you can add it here
+
+    alert('Registration successful!');
+});
